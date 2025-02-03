5d0751cfd18ace56671fcd7356e28dae43afd0ea
Craig Sloggett <craigsloggett@gmail.com>
2025-02-03 14:39:46 -0500

    ci: Get previous commit sha

--- .github/scripts/wait_for_check.sh
+++ .github/scripts/wait_for_check.sh
@@ -8,9 +8,10 @@ COMMIT_SHA="${COMMIT_SHA:?Please set the COMMIT_SHA environment variable and run
 
 owner="craigsloggett-lab"
 repo="terraform-aws-tfe-fdo-docker-active-active"
+previous_commit_sha="${COMMIT_SHA}~1"
 
 curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  "https://api.github.com/repos/${owner}/${repo}/commits//check-runs"
+  "https://api.github.com/repos/${owner}/${repo}/commits/${previous_commit_sha}/check-runs"
